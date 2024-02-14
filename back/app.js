858fa55b7c8a21b053f0a73789cdacb3fca04d31
YounesAH15 <121797352+YounesAH15@users.noreply.github.com>
2024-02-14 17:23:10 +0100

    Update app.js

--- back/app.js
+++ back/app.js
@@ -1,1 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mgdb = require('mongodb')
+const client = mgdb.MongoClient 
+
+const psw = "uhw8inVMxDRBtgmw"
+const url = "mongodb+srv://younesharrat5896:"+psw+"@cluster-alten.qqnnpgu.mongodb.net/?retryWrites=true&w=majority"
+
+
+client.connect(url, (err, c) => {
+
+  console.log("yes connected", {
+    client: c,
+  })
+  
+  
+})
+
+
+
+app.listen(port)
 
